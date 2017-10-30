6f0748657846dce65daca5e569867836b942ebd9
Daniel Knight <Daniel Knight>
2017-10-30 14:54:04 -0400

    Add .NET Web Forms project for new UI

---
+++ TalentShowWeb/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TalentShowWeb.Startup))]
+namespace TalentShowWeb
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
