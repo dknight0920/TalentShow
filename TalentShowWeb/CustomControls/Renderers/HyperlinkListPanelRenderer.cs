e3b44d48a23e92b3ef7d88ba970d1e5824adde0e
Daniel Knight <Daniel Knight>
2017-11-13 22:07:33 -0500

    Hide Add buttons if user is not an admin

--- TalentShowWeb/CustomControls/Renderers/HyperlinkListPanelRenderer.cs
+++ TalentShowWeb/CustomControls/Renderers/HyperlinkListPanelRenderer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI.WebControls;
+using TalentShowWeb.Account.Util;
 using TalentShowWeb.CustomControls.Models;
 
 namespace TalentShowWeb.CustomControls.Renderers
@@ -11,7 +12,7 @@ namespace TalentShowWeb.CustomControls.Renderers
     {
         public static void Render(HyperlinkListPanel Panel, HyperlinkListPanelConfig Config)
         {
-            Panel.ShowAddButton = Config.ButtonClickHandler == null ? false : true;
+            Panel.ShowAddButton = (Config.ButtonClickHandler == null ? false : true) && new AccountUtil(HttpContext.Current).IsUserAnAdmin();
 
             if (Panel.ShowAddButton)
                 Panel.GetPanelAddButton().Click += new EventHandler(Config.ButtonClickHandler);
