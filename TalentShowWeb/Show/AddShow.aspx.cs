9b177c89039cecae2e0c3728926b6471ac36fad5
Daniel Knight <Daniel Knight>
2017-12-01 14:35:45 -0500

    Add show stress testing

--- TalentShowWeb/Show/AddShow.aspx.cs
+++ TalentShowWeb/Show/AddShow.aspx.cs
@@ -47,14 +47,26 @@ namespace TalentShowWeb.Show
             var description = showForm.GetDescriptionTextBox().Text.Trim();
             var show = new TalentShow.Show(0, showName, description);
             ServiceFactory.ShowService.Add(show);
+            ImportContests(show);
 
+            GoToShowsPage();
+        }
+
+        private void ImportContests(TalentShow.Show show)
+        {
             var util = new BrushfireAttendeeDocumentUtil(new System.IO.DirectoryInfo(@"C:\Users\Daniel\Documents"), @"\KNIGHTREPORT2017.xlsx");
             var brushFireContests = util.GetContests();
+            var users = new AccountUtil(Context).GetAllUsers().Take(4);
+            var judges = new List<TalentShow.Judge>();
 
-            foreach(var brushFireContest in brushFireContests)
+            foreach (var user in users)
+                judges.Add(new TalentShow.Judge(id: 0, userId: user.Id));
+
+            foreach (var brushFireContest in brushFireContests)
             {
                 if (brushFireContest.Contestants.Count() == 0) continue;
 
+                //Add Contest
                 var contest = new TalentShow.Contest(
                     id: 0,
                     name: brushFireContest.Name,
@@ -66,6 +78,24 @@ namespace TalentShowWeb.Show
 
                 ServiceFactory.ContestService.AddShowContest(show.Id, contest);
 
+                //Add Judges to Contest
+                foreach (var judge in judges)
+                    ServiceFactory.JudgeService.AddContestJudge(contest.Id, judge);
+
+                //Add Score Criteria to Contest
+                var scoreCriteria = new List<TalentShow.ScoreCriterion>()
+                {
+                    new TalentShow.ScoreCriterion(id: 0, criterionDescription: "Selection & Communication", scoreRange: new TalentShow.ScoreRange(0, 10)),
+                    new TalentShow.ScoreCriterion(id: 0, criterionDescription: "Technique, Intonation", scoreRange: new TalentShow.ScoreRange(0, 10)),
+                    new TalentShow.ScoreCriterion(id: 0, criterionDescription: "Overall Presentation", scoreRange: new TalentShow.ScoreRange(0, 10)),
+                    new TalentShow.ScoreCriterion(id: 0, criterionDescription: "Diction", scoreRange: new TalentShow.ScoreRange(0, 10)),
+                    new TalentShow.ScoreCriterion(id: 0, criterionDescription: "Musical Effect Communication", scoreRange: new TalentShow.ScoreRange(0, 10)),
+                };
+
+                foreach (var scoreCriterion in scoreCriteria)
+                    ServiceFactory.ScoreCriterionService.AddContestScoreCriterion(contest.Id, scoreCriterion);
+
+                //Add Contestants to Contest
                 foreach (var brushFireContestant in brushFireContest.Contestants)
                 {
                     var contestant = new TalentShow.Contestant(
@@ -80,6 +110,7 @@ namespace TalentShowWeb.Show
 
                     ServiceFactory.ContestantService.AddContestContestant(contest.Id, contestant);
 
+                    //Add Performers to Contestant
                     foreach (var brushFirePerformer in brushFireContestant.Performers)
                     {
                         int divisionId = 1085;
@@ -96,10 +127,26 @@ namespace TalentShowWeb.Show
                             )
                         );
                     }
+
+                    //Add Score Cards to Contestant
+                    var scorableCriteria = new List<TalentShow.ScorableCriterion>();
+
+                    foreach (var scoreCriterion in scoreCriteria)
+                        scorableCriteria.Add(new TalentShow.ScorableCriterion(id: 0, scoreCriterion: scoreCriterion));
+
+                    foreach (var judge in judges)
+                    {
+                        ServiceFactory.ScoreCardService.Add(
+                            new TalentShow.ScoreCard(
+                                id: 0,
+                                contestant: contestant,
+                                judge: judge,
+                                scorableCriteria: scorableCriteria
+                            )
+                        );
+                    }
                 }
             }
-
-            GoToShowsPage();
         }
 
         protected void btnCancel_Click(object sender, EventArgs e)
