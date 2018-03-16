3b21f7aea4fd5f52aa9fc1a0e6215a14edd5984d
Daniel Knight <daniel.knight@rmbsolutions.net>
2018-03-15 21:56:54 -0400

    Started to enable forgot password feature

--- TalentShowWeb/Account/Forgot.aspx.cs
+++ TalentShowWeb/Account/Forgot.aspx.cs
@@ -20,8 +20,8 @@ namespace TalentShowWeb.Account
             {
                 // Validate the user's email address
                 var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
-                ApplicationUser user = manager.FindByName(Email.Text);
-                if (user == null || !manager.IsEmailConfirmed(user.Id))
+                ApplicationUser user = manager.FindByEmail(Email.Text);
+                if (user == null)
                 {
                     FailureText.Text = "The user either does not exist or is not confirmed.";
                     ErrorMessage.Visible = true;
@@ -29,9 +29,9 @@ namespace TalentShowWeb.Account
                 }
                 // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                 // Send email with the code and the redirect to reset password page
-                //string code = manager.GeneratePasswordResetToken(user.Id);
-                //string callbackUrl = IdentityHelper.GetResetPasswordRedirectUrl(code, Request);
-                //manager.SendEmail(user.Id, "Reset Password", "Please reset your password by clicking <a href=\"" + callbackUrl + "\">here</a>.");
+                string code = manager.GeneratePasswordResetToken(user.Id);
+                string callbackUrl = IdentityHelper.GetResetPasswordRedirectUrl(code, Request);
+                manager.SendEmail(user.Id, "Reset Password", "Please reset your password by clicking <a href=\"" + callbackUrl + "\">here</a>.");
                 loginForm.Visible = false;
                 DisplayEmail.Visible = true;
             }
